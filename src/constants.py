d58d0a1986b0e1a49abea51ae6b3a6ed066df058
niragui <ignacioiragu2@gmail.com>
2025-06-24 00:43:20 -0300

    fix: remove double / from urls

--- src/constants.py
+++ src/constants.py
@@ -5,9 +5,9 @@ BASE_SIMPLER_API_URL = "https://api.music.apple.com/v1/catalog/"
 BASE_APPLE_URL = "https://music.apple.com/"
 
 
-API_URL = f"{BASE_API_URL}/us/"
-SIMPLER_API_URL = f"{BASE_SIMPLER_API_URL}/us/"
-APPLE_URL = f"{BASE_APPLE_URL}/us/"
+API_URL = f"{BASE_API_URL}us/"
+SIMPLER_API_URL = f"{BASE_SIMPLER_API_URL}us/"
+APPLE_URL = f"{BASE_APPLE_URL}us/"
 
 
 SRC_FOLDER = os.path.dirname(__file__)
